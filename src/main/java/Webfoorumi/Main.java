af4fa0bb9c5d5ca3e327b466c1568ef9b9180eb7
Aleksi <Aleksi@Pce.dhcp.inet.fi>
2016-02-26 18:58:57 +0200

    templaatit

--- src/main/java/Webfoorumi/Main.java
+++ src/main/java/Webfoorumi/Main.java
@@ -5,12 +5,26 @@
  */
 package Webfoorumi;
 
+import java.util.HashMap;
+import java.util.List;
+import spark.ModelAndView;
+import static spark.Spark.get;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
+
 /**
  *
  * @author Aleksi
  */
 public class Main {
-    public void main(String[] args){
-        System.out.println("hei");
+
+    public static void main(String[] args) throws Exception {
+        
+        get("/sivu", (req, res) -> {
+
+            HashMap map = new HashMap<>();
+
+            return new ModelAndView(map, "index");
+        }, new ThymeleafTemplateEngine());
     }
+
 }

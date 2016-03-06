3d806e9bc6a6433602e9f483a5df307b38966af9
Sopulius <aleksi.suoranta@gmail.com>
2016-03-06 20:26:34 +0200

    Kayttajadao

--- src/main/java/Webfoorumi/DAO/KayttajaDAO.java
+++ src/main/java/Webfoorumi/DAO/KayttajaDAO.java
@@ -5,10 +5,84 @@
  */
 package Webfoorumi.DAO;
 
+import Webfoorumi.Database.Database;
+import Webfoorumi.Dom.Kayttaja;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author jappappi
  */
-public class KayttajaDAO {
+public class KayttajaDAO implements Dao<Kayttaja, Integer> {
+    Database database;
     
+    public KayttajaDAO(Database db){
+        this.database = db;
+    }
+
+    @Override
+    public Kayttaja findOne(Integer key) throws SQLException {
+        
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Kayttaja WHERE Kayttaja.id = ?");
+        stmt.setObject(1, key);
+
+        ResultSet rs = stmt.executeQuery();
+        boolean hasOne = rs.next();
+        if (!hasOne) {
+            return null;
+        }
+
+        int id = rs.getInt("id");
+        String nimi = rs.getString("nimi");
+
+        Kayttaja kayttaja = new Kayttaja(id, nimi);
+
+        rs.close();
+        stmt.close();
+        connection.close();
+
+        return kayttaja;
+    }
+
+    @Override
+    public List<Kayttaja> findAll() throws SQLException {
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Kayttaja");
+        ResultSet rs = stmt.executeQuery();
+
+        List<Kayttaja> kayttajat = new ArrayList<>();
+
+        while (rs.next()) {
+
+            int id = rs.getInt("id");
+            String nimi = rs.getString("nimi");
+
+            Kayttaja a = new Kayttaja(id, nimi);
+            kayttajat.add(a);
+        }
+
+        rs.close();
+        stmt.close();
+        connection.close();
+
+        return kayttajat;
+    }
+
+    @Override
+    public void delete(Integer key) throws SQLException {
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("DELETE FROM Kayttaja WHERE Kayttaja.id = ?");
+        stmt.setObject(1, key);
+        int tulos = stmt.executeUpdate();
+        System.out.println(tulos);
+
+        stmt.close();
+        connection.close();
+    }
 }

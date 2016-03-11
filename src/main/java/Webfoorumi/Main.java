456e16c292020306548d698d6b36985f2c4d1026
Sopulius <aleksi.suoranta@gmail.com>
2016-03-11 21:01:15 +0200

    keskustelunlisäys

--- src/main/java/Webfoorumi/Main.java
+++ src/main/java/Webfoorumi/Main.java
@@ -10,6 +10,9 @@ import Webfoorumi.DAO.KayttajaDAO;
 import Webfoorumi.DAO.KeskusteluDAO;
 import Webfoorumi.Database.Database;
 import Webfoorumi.Dom.Keskustelu;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.HashMap;
 import java.util.List;
 import spark.ModelAndView;
@@ -57,7 +60,35 @@ public class Main  {
            return new ModelAndView(map, "alue");
         }, new ThymeleafTemplateEngine());
         //todo uuden keskustelun lisäys(luomiselle oma sivu?)
-        
+        post("/alue/:id", (req,res) -> {
+            String otsikko = req.queryParams("otsikko");
+            String viesti = req.queryParams("viesti");
+            String nimi = req.queryParams("nimi");
+            
+            database.update("INSERT INTO Kayttaja(nimi) VALUES(?)",nimi);
+            Connection c = database.getConnection();
+            PreparedStatement stmt = c.prepareStatement("SELECT * FROM Kayttaja WHERE id = (SELECT MAX(id) FROM Kayttaja)");
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+            int kayttaja = rs.getInt("id");
+            c.close();
+            rs.close();
+            
+            database.update("INSERT INTO Keskustelu(nimi,alue_id,aloittaja_id) VALUES(?,?,?)",otsikko,Integer.parseInt(req.params("id")),kayttaja);
+            c = database.getConnection();
+            stmt = c.prepareStatement("SELECT * FROM Keskustelu WHERE id = (SELECT MAX(id) FROM Keskustelu)");
+            rs = stmt.executeQuery();
+            rs.next();
+            int k_id = rs.getInt("id");
+            c.close();
+            rs.close();
+            
+            database.update("INSERT INTO Viesti(sisalto, keskustelu_id, lahettaja_id, vastaus_id) VALUES(?,?,?,?)", viesti, k_id, kayttaja , null);
+                
+            
+            res.redirect(""+Integer.parseInt(req.params("id")));
+            return null;
+        });
         //todo näytä keskustelun viestit
     }
 
